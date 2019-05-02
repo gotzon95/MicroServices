5dbce68903efdb72852e2df3097c20b927254b4f
Xavier Iborra Sanchez <xiborra@certiorem.com>
2019-05-02 10:55:23 +0200

    Create project ModalDataService

---
+++ ModelDataService/ModelDataService/src/main/java/com/certiorem/microservices/ModelDataService/App.java
@@ -0,0 +1,13 @@
+package com.certiorem.microservices.ModelDataService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
